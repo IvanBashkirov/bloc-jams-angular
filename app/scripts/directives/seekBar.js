9fb173905c2b320df21a0c49fff1159274c8e294
Ivan <ivanbashkirov2@tutanota.com>
2017-01-05 18:18:08 +0300

    checkpoint ang-jams-11 completed

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -76,7 +76,6 @@
         scope.onClickSeekBar = function (event) {
           var percent = calculatePercent(seekBar, event);
           scope.value = percent * scope.max;
-          console.log(scope.value);
           notifyOnChange(scope.value);
         };
       }
